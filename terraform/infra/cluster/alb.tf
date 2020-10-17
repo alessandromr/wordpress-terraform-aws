47815bc3dd2b7ee3588ae44abc59d3657e5aea06
Alessandro Marino <alessandromarinoac@gmail.com>
2020-10-17 22:59:43 +0200

    Fix alb on cluster

--- terraform/infra/cluster/alb.tf
+++ terraform/infra/cluster/alb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "load_balancer" {
-  name               = "${local.prefix}-${terraform.workspace}-alb"
+  name               = "${local.prefix}-${terraform.workspace}"
   internal           = false
   load_balancer_type = "application"
 
@@ -25,7 +25,7 @@ resource "aws_lb_listener" "default_listener" {
 }
 
 resource "aws_security_group" "alb" {
-  name        = "${local.prefix}-${terraform.workspace}-ecs-instances-security-group"
+  name        = "${local.prefix}-${terraform.workspace}-ecs-alb-security-group"
   description = "Allow TLS inbound traffic"
   vpc_id      = local.vpc_id
 }
@@ -47,12 +47,11 @@ resource "aws_security_group_rule" "alb_allow_inbound_https_from_internet" {
   security_group_id = aws_security_group.alb.id
 }
 
-resource "aws_security_group_rule" "alb_out" {
+resource "aws_security_group_rule" "alb_out_to_ecs" {
   type                     = "egress"
   from_port                = 0
   to_port                  = 0
   protocol                 = "-1"
-  cidr_blocks              = ["0.0.0.0/0"]
   source_security_group_id = aws_security_group.ecs_instances.id
   security_group_id        = aws_security_group.alb.id
 }
