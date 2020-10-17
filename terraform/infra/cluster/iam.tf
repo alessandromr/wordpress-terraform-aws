190f0f25af455b47c2969eaa5f97367633f78227
Alessandro Marino <alessandromarinoac@gmail.com>
2020-10-18 00:19:31 +0200

    Add ssm role to ecs_instances

--- terraform/infra/cluster/iam.tf
+++ terraform/infra/cluster/iam.tf
@@ -32,3 +32,8 @@ resource "aws_iam_role_policy_attachment" "ecs_ec2_cloudwatch_role" {
   role       = aws_iam_role.ecs_instances.id
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
+
+resource "aws_iam_role_policy_attachment" "ecs_ec2_ssm_role" {
+  role       = aws_iam_role.ecs_instances.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
