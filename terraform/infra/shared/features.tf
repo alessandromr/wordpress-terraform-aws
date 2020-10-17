46297f5396a2a1c1f9e41a871632030f3f880bd8
Alessandro Marino <alessandromarinoac@gmail.com>
2020-10-17 19:51:53 +0200

    Fix shared stack for deploy

--- terraform/infra/shared/features.tf
+++ terraform/infra/shared/features.tf
@@ -1,14 +1,14 @@
 module "basic_infra" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "2.58.0"
 
-  name = "${local.prefix}"
-  cidr = "10.0.0.0/22"
+  name = "${local.prefix}-${terraform.workspace}"
+  cidr = "10.0.0.0/20"
 
-  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  database_subnets = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
+  azs              = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  private_subnets  = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets   = ["10.0.5.0/24", "10.0.6.0/24", "10.0.7.0/24"]
+  database_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = false
--- terraform/infra/shared/outputs.tf
+++ terraform/infra/shared/outputs.tf
@@ -1,7 +1,6 @@
 output "vpc_id" {
   value = module.basic_infra.vpc_id
 }
-
 output "public_subnets_ids" {
   value = module.basic_infra.public_subnets
 }
@@ -10,4 +9,7 @@ output "private_subnets_ids" {
 }
 output "database_subnets_ids" {
   value = module.basic_infra.database_subnets
+}
+output "database_subnets_group" {
+  value = module.basic_infra.database_subnet_group
 }
