13832f84dfc8fef8345e2e1c42b088cadda5bec2
Alessandro Marino <alessandromarinoac@gmail.com>
2020-10-20 01:47:32 +0200

    Add output

--- terraform/infra/cluster/outputs.tf
+++ terraform/infra/cluster/outputs.tf
@@ -6,6 +6,10 @@ output "ecs_cluster_arn" {
   value = aws_ecs_cluster.ecs_wordpress.arn
 }
 
+output "ecs_instances_sg_id" {
+  value = aws_security_group.ecs_instances.id
+}
+
 #alb
 output "aws_alb_arn" {
   value = aws_lb.load_balancer.arn
