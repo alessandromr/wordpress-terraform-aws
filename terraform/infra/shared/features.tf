f157727edd3fc5c947faf4fb887754b3465b8f53
Alessandro Marino <alessandromarinoac@gmail.com>
2020-10-20 03:08:24 +0200

    enable_dns_hostnames

--- terraform/infra/shared/features.tf
+++ terraform/infra/shared/features.tf
@@ -10,6 +10,9 @@ module "basic_infra" {
   public_subnets   = ["10.0.5.0/24", "10.0.6.0/24", "10.0.7.0/24"]
   database_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
 
+  enable_dns_hostnames=true
+  enable_dns_support=true
+
   enable_nat_gateway = true
   enable_vpn_gateway = false
 
