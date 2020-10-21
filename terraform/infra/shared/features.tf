ca27df820b3293ecb61d1622b9948b1a00f51aa5
Alessandro Marino <alessandromarinoac@gmail.com>
2020-10-21 10:35:28 +0200

    Wider subnets for privates

--- terraform/infra/shared/features.tf
+++ terraform/infra/shared/features.tf
@@ -6,9 +6,9 @@ module "basic_infra" {
   cidr = "10.0.0.0/20"
 
   azs              = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
-  private_subnets  = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets   = ["10.0.5.0/24", "10.0.6.0/24", "10.0.7.0/24"]
-  database_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  private_subnets  = ["10.0.0.0/23", "10.0.2.0/23", "10.0.4.0/23"]
+  public_subnets   = ["10.0.8.0/24", "10.0.9.0/24", "10.0.10.0/24"]
+  database_subnets = ["10.0.12.0/24", "10.0.13.0/24", "10.0.14.0/24"]
 
   enable_dns_hostnames = true
   enable_dns_support   = true
